71b949778fa7ed047dacfd2c87e489b58dcaf4ac
Pavan Belagatti <pavan@shippable.com>
2022-11-16 17:32:42 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.use(require('express-status-monitor')())
-
 app.get("/hello", (_req, res) =>  {
   res.status(200).send("Hello World!")
 })
